72ff4c718089c17df386eee60e4fbf7fb6869da0
Monir <mrmonir0558@gmail.com>
2025-01-08 17:30:40 +0600

    added adminRoute for use verify admin

--- src/Routes/PrivetRoute.jsx
+++ src/Routes/PrivetRoute.jsx
@@ -1,10 +1,10 @@
-import { useContext } from "react";
-import { AuthContext } from "../providers/AuthProvider";
+
 import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const PrivetRoute = ({ children }) => {
 
-    const { user, loading } = useContext(AuthContext)
+    const { user, loading } = useAuth()
     const location = useLocation()
 
     if (loading) {
@@ -15,8 +15,7 @@ const PrivetRoute = ({ children }) => {
         return children;
     }
     
-    return <Navigate to='/login' state={{from: location}} replace />
-        
+    return <Navigate to='/login' state={{ from: location }} replace />
     
 };
 
